87fe582ad2cc540a376e5e109d944541cee3ac89
lena-kovaleva <lena-mikhajlova@yandex.ru>
2019-02-13 23:34:40 +0100

    improved Switcher

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Switcher = ({ label, items, value, className }) => (
-    <div className={ `switcher ${className}` }>
-        <span className="switcher__label">{ label }</span>
-        {
-            items.map(
-                item => <a key={item} className={ `switcher__button ${item === value ? 'switcher__button--active' : ''}` } href="">{ item }</a>
-            )
-        }
-    </div>
-)
+class Switcher extends Component {
+    state = {
+        value: this.props.value
+    }
+
+    setValue = (value) => {
+        this.setState({ value });
+    }
+
+    render() {
+        const { label, items, className } = this.props;
+        const { value } = this.state;
+
+        return (
+            <div className={ `switcher ${className}` }>
+                <span className="switcher__label">{ label }</span>
+                {
+                    items.map(
+                        item => <a key={item} onClick={ () => this.setValue(item) } className={ `switcher__button ${item === value ? 'switcher__button--active' : ''}` }>{ item }</a>
+                    )
+                }
+            </div>
+        )
+    }
+}
 
 export default Switcher;
